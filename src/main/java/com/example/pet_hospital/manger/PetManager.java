3f07279906c62212732f007e7972b60dc37d80a0
NinhPc <ninhmphpl@gmail.com>
2022-12-05 20:54:24 +0700

    up file

--- src/main/java/com/example/pet_hospital/manger/PetManager.java
+++ src/main/java/com/example/pet_hospital/manger/PetManager.java
@@ -11,20 +11,5 @@ import java.io.IOException;
 import java.util.List;
 
 public class PetManager {
-    public void showPetList(HttpServletRequest request, HttpServletResponse response){
-        //lấy dữ liệu Pet từ DAO gán cho attribute chuyển tiếp cho trang cần hiển thị
-    }
-    public void petDetail(HttpServletRequest request,HttpServletResponse response){
-        //(parameter: id)
-        // Lấy dữ liệu pet từ DAO , tìm đối tương pets có id trung khớp
-        // gán đối tượng đó cho attribute chuyên tiếp cho trang hiển thị
-    }
-
-    public void findAllPets(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PetDAO petDAO = new PetDAO();
-        List<Pets> pets = petDAO.findAllPet(request);
-        request.setAttribute("pets", pets);
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("shop-product.jsp");
-        requestDispatcher.forward(request, response);
-    }
+//sơn
 }
